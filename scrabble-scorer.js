ef2e916776464e962d06f9a634686d3bd6fb5b97
Billie-LS <lskylizard@yahoo.com>
2024-02-03 15:12:27 -0600

    3 green, 14 red = fail

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -15,16 +15,27 @@ const oldPointStructure = {
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
+
+   // variable to sum up total numeric score
+   let totalScore = 0;
+
+
    for (let i = 0; i < word.length; i++) {
       
       for (const pointValue in oldPointStructure) {
          
          if (oldPointStructure[pointValue].includes(word[i])) {
 			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+
+         totalScore += 1;  // Increment the total score
       }
    }
 }
-return letterPoints;
+// return letterPoints;
+return {
+   letterPoints: letterPoints,
+   totalScore: totalScore
+};
 }
 
 // TODO: your job is to finish writing these functions and variables that we've named //
@@ -34,19 +45,62 @@ return letterPoints;
 
 function initialPrompt() {
    let word = input.question("Let's play some scrabble! Enter a word: ");
-   return word;
+
+   // Score input word with oldScrabbleScorer function call
+   const result = oldScrabbleScorer(word);
+   
+   // console.log(result);
+
+   // print scoring output
+   console.log(result.letterPoints); // Print letterPoints
+   
+   console.log(`Total Score: ${result.totalScore}`);// Print totalScore
+
+   return result;
 
 };
 
+/*
+TASK 2: ADD AND ORGANIZE SCORING ALGORITHMS
+Your job here is to write two other scoring algorithms for the Scrabble player.
+*/
+
+// TODO: Define a function that takes a word as a parameter and returns a numerical score. 
+// Each letter within the word is worth 1 point.
+
+function simpleScorer(word){
+	word = word.toUpperCase();
+	let letterPoints = "";
+   // variable to sum up total numeric score
+   let totalScore = 0;
+
+   for (let i = 0; i < word.length; i++) {
+      letterPoints += `Points for '${word[i]}': 1\n`;
+      totalScore += 1;  // Increment the total score
+   }
+   
+   console.log(`Total Score: ${totalScore}`);  // print out total score
+
+   // Return object contain letterPoints and totalScore
+   return {
+      letterPoints: letterPoints,
+      totalScore: totalScore
+   };
+};
 
 
-let simpleScorer;
+// TODO: Define a function that takes a word as a parameter and returns a score. 
+// Each vowel within the word is worth 3 points, and each consonant is worth 1 point.
+function vowelBonusScorer(){
+   
+}
 
-let vowelBonusScorer;
+
 
 // TODO: Use the oldScrabbleScorer() function provided to score the word provided by the user. 
 // Print the result to the console.
 let scrabbleScorer = oldScrabbleScorer;
+
 
 const scoringAlgorithms = [];
 
